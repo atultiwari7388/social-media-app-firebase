935aaee7cad048c5471c78ab86ee57910e19127d
Atul Tiwari <tiwariatul9526@gmail.com>
2022-01-17 18:27:24 +0530

    store posts to databse

--- lib/resources/storage_method.resources.dart
+++ lib/resources/storage_method.resources.dart
@@ -18,7 +18,7 @@ class StorageMethods {
 
     if (isPost) {
       String id = const Uuid().v1();
-      ref.child(id);
+      ref = ref.child(id);
     }
 
     UploadTask uploadTask = ref.putData(file);
